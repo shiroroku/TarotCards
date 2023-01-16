f6dfc5bc707e157ca3bbb6110c429dd4b57d1cb4
Shiroroku(Elise) <elseytd@gmail.com>
2023-01-16 00:16:44 -0800

    organized config

--- src/main/java/shiroroku/tarotcards/Configuration.java
+++ src/main/java/shiroroku/tarotcards/Configuration.java
@@ -6,14 +6,15 @@ public class Configuration {
 
 	public static ForgeConfigSpec config;
 
+	public static ForgeConfigSpec.BooleanValue do_loot_generation;
 	public static ForgeConfigSpec.DoubleValue death_damagebonus;
+	public static ForgeConfigSpec.DoubleValue default_loot_chance;
 	public static ForgeConfigSpec.DoubleValue judgement_damagechance;
 	public static ForgeConfigSpec.DoubleValue temperance_reduction;
 	public static ForgeConfigSpec.DoubleValue the_chariot_speedboost;
 	public static ForgeConfigSpec.DoubleValue the_hermit_allyrange;
 	public static ForgeConfigSpec.DoubleValue the_hermit_armorbonus;
 	public static ForgeConfigSpec.DoubleValue the_hierophant_xpboost;
-	public static ForgeConfigSpec.IntValue the_highpriestess_upgradecost;
 	public static ForgeConfigSpec.DoubleValue the_lovers_range;
 	public static ForgeConfigSpec.DoubleValue the_star_reachboost;
 	public static ForgeConfigSpec.DoubleValue the_sun_healthboost;
@@ -21,12 +22,20 @@ public class Configuration {
 	public static ForgeConfigSpec.IntValue strength_amplifier;
 	public static ForgeConfigSpec.IntValue the_devil_weaknessamplifier;
 	public static ForgeConfigSpec.IntValue the_emperpor_heroofvillagebonus;
+	public static ForgeConfigSpec.IntValue the_fool_jumpboost;
+	public static ForgeConfigSpec.IntValue the_highpriestess_upgradecost;
 	public static ForgeConfigSpec.IntValue the_lovers_regenamplifier;
+	public static ForgeConfigSpec.IntValue tick_rate;
 
 	static {
 		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 		builder.push("Tarot Cards");
-
+		tick_rate = builder.comment("How many ticks it takes to check if a player has a tarot or not, more = more performace but longer time to activate/deactivate effects").defineInRange("tick_rate", 20, 0, 1200);
+		builder.push("Loot");
+		default_loot_chance = builder.comment("Chance a tarot card appears in default loot (0.75 = 75%)(1.0 = there will always be a single tarot card in chests)").defineInRange("default_loot_chance", 0D, 0.75D, 1D);
+		do_loot_generation = builder.comment("If Tarot Cards should be added to the default loot tables specified in data").define("do_loot_generation", true);
+		builder.pop();
+		builder.push("Cards");
 		death_damagebonus = builder.comment("Percentage of damage increase to living (0.5 = +50%)").defineInRange("death_damagebonus", 0.5D, 0D, 100D);
 		judgement_damagechance = builder.comment("Percentage chance of dealing double damage (0.5 = 50%)").defineInRange("judgement_damagechance", 0.5D, 0D, 1D);
 		strength_amplifier = builder.comment("Amplifier for effect (1 = II)").defineInRange("strength_amplifier", 1, 0, 20);
@@ -34,6 +43,7 @@ public class Configuration {
 		the_chariot_speedboost = builder.comment("Percentage increase of base speed (0.5 = +50%)").defineInRange("the_chariot_speedboost", 0.5D, 0.0D, 100D);
 		the_devil_weaknessamplifier = builder.comment("Amplifier for effect (1 = II)").defineInRange("the_devil_weaknessamplifier", 2, 0, 20);
 		the_emperpor_heroofvillagebonus = builder.comment("Amplifier for effect (1 = II)").defineInRange("the_emperpor_heroofvillagebonus", 2, 0, 20);
+		the_fool_jumpboost = builder.comment("Amplifier for effect (2 = III)").defineInRange("the_fool_jumpboost", 2, 0, 100);
 		the_hermit_allyrange = builder.comment("How far away the player must be from allies to activate").defineInRange("the_hermit_allyrange", 10D, 0.0D, 256D);
 		the_hermit_armorbonus = builder.comment("How much armor is added when away from allies").defineInRange("the_hermit_armorbonus", 10D, 0.0D, 100D);
 		the_hierophant_xpboost = builder.comment("Percentage increase of experience (2 = +200%)").defineInRange("the_hierophant_xpboost", 2D, 1D, 100D);
@@ -43,7 +53,7 @@ public class Configuration {
 		the_star_reachboost = builder.comment("Percentage increase of reach distance (0.5 = +50%)").defineInRange("the_star_reachboost", 0.5D, 0.0D, 100D);
 		the_sun_healthboost = builder.comment("Percentage increase of base health (0.5 = +50%)").defineInRange("the_sun_healthboost", 0.5D, 0.0D, 100D);
 		wheel_of_fortune_luckbonus = builder.comment("How much luck (not fortune) is added to the player").defineInRange("wheel_of_fortune_luckbonus", 3D, 0.0D, 100D);
-
+		builder.pop();
 		builder.pop();
 		config = builder.build();
 	}
