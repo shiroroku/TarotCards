7f002ddd8e5868129cd74911f2a0d62688c2395e
Shiroroku(Elise) <elseytd@gmail.com>
2023-01-16 00:25:17 -0800

    config up to date

--- src/main/java/shiroroku/tarotcards/Configuration.java
+++ src/main/java/shiroroku/tarotcards/Configuration.java
@@ -12,12 +12,14 @@ public class Configuration {
 	public static ForgeConfigSpec.DoubleValue judgement_damagechance;
 	public static ForgeConfigSpec.DoubleValue temperance_reduction;
 	public static ForgeConfigSpec.DoubleValue the_chariot_speedboost;
+	public static ForgeConfigSpec.DoubleValue the_hanged_man_xpratio;
 	public static ForgeConfigSpec.DoubleValue the_hermit_allyrange;
 	public static ForgeConfigSpec.DoubleValue the_hermit_armorbonus;
 	public static ForgeConfigSpec.DoubleValue the_hierophant_xpboost;
 	public static ForgeConfigSpec.DoubleValue the_lovers_range;
 	public static ForgeConfigSpec.DoubleValue the_star_reachboost;
 	public static ForgeConfigSpec.DoubleValue the_sun_healthboost;
+	public static ForgeConfigSpec.DoubleValue the_world_range;
 	public static ForgeConfigSpec.DoubleValue wheel_of_fortune_luckbonus;
 	public static ForgeConfigSpec.IntValue strength_amplifier;
 	public static ForgeConfigSpec.IntValue the_devil_weaknessamplifier;
@@ -25,6 +27,7 @@ public class Configuration {
 	public static ForgeConfigSpec.IntValue the_fool_jumpboost;
 	public static ForgeConfigSpec.IntValue the_highpriestess_upgradecost;
 	public static ForgeConfigSpec.IntValue the_lovers_regenamplifier;
+	public static ForgeConfigSpec.IntValue the_world_slownessamplifier;
 	public static ForgeConfigSpec.IntValue tick_rate;
 
 	static {
@@ -32,7 +35,7 @@ public class Configuration {
 		builder.push("Tarot Cards");
 		tick_rate = builder.comment("How many ticks it takes to check if a player has a tarot or not, more = more performace but longer time to activate/deactivate effects").defineInRange("tick_rate", 20, 0, 1200);
 		builder.push("Loot");
-		default_loot_chance = builder.comment("Chance a tarot card appears in default loot (0.75 = 75%)(1.0 = there will always be a single tarot card in chests)").defineInRange("default_loot_chance", 0D, 0.75D, 1D);
+		default_loot_chance = builder.comment("Chance a tarot card appears in default loot (0.75 = 75%)(1.0 = there will always be a single tarot card in chests)").defineInRange("default_loot_chance", 0.75D, 0D, 1D);
 		do_loot_generation = builder.comment("If Tarot Cards should be added to the default loot tables specified in data").define("do_loot_generation", true);
 		builder.pop();
 		builder.push("Cards");
@@ -44,6 +47,7 @@ public class Configuration {
 		the_devil_weaknessamplifier = builder.comment("Amplifier for effect (1 = II)").defineInRange("the_devil_weaknessamplifier", 2, 0, 20);
 		the_emperpor_heroofvillagebonus = builder.comment("Amplifier for effect (1 = II)").defineInRange("the_emperpor_heroofvillagebonus", 2, 0, 20);
 		the_fool_jumpboost = builder.comment("Amplifier for effect (2 = III)").defineInRange("the_fool_jumpboost", 2, 0, 100);
+		the_hanged_man_xpratio = builder.comment("How much of the damage is converted to experience").defineInRange("the_hanged_man_xpratio", 0.5D, 0.0D, 1D);
 		the_hermit_allyrange = builder.comment("How far away the player must be from allies to activate").defineInRange("the_hermit_allyrange", 10D, 0.0D, 256D);
 		the_hermit_armorbonus = builder.comment("How much armor is added when away from allies").defineInRange("the_hermit_armorbonus", 10D, 0.0D, 100D);
 		the_hierophant_xpboost = builder.comment("Percentage increase of experience (2 = +200%)").defineInRange("the_hierophant_xpboost", 2D, 1D, 100D);
@@ -52,6 +56,8 @@ public class Configuration {
 		the_lovers_regenamplifier = builder.comment("Amplifier for effect (1 = II)").defineInRange("the_lovers_regenamplifier", 2, 0, 20);
 		the_star_reachboost = builder.comment("Percentage increase of reach distance (0.5 = +50%)").defineInRange("the_star_reachboost", 0.5D, 0.0D, 100D);
 		the_sun_healthboost = builder.comment("Percentage increase of base health (0.5 = +50%)").defineInRange("the_sun_healthboost", 0.5D, 0.0D, 100D);
+		the_world_range = builder.comment("How close non-allies need to be to apply slowness").defineInRange("the_world_range", 3D, 0.0D, 256D);
+		the_world_slownessamplifier = builder.comment("Amplifier for effect (9 = X)").defineInRange("the_world_slownessamplifier", 9, 0, 20);
 		wheel_of_fortune_luckbonus = builder.comment("How much luck (not fortune) is added to the player").defineInRange("wheel_of_fortune_luckbonus", 3D, 0.0D, 100D);
 		builder.pop();
 		builder.pop();
