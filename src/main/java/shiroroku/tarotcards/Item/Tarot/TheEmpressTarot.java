35321d53b74378419f31cb502f15de27d75d25b2
Shiroroku(Elise) <elseytd@gmail.com>
2023-01-20 18:31:24 -0800

    added empress ability

---
+++ src/main/java/shiroroku/tarotcards/Item/Tarot/TheEmpressTarot.java
@@ -0,0 +1,32 @@
+package shiroroku.tarotcards.Item.Tarot;
+
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.TickEvent;
+import shiroroku.tarotcards.Configuration;
+import shiroroku.tarotcards.Item.TarotItem;
+import shiroroku.tarotcards.Registry.ItemRegistry;
+
+import java.util.List;
+
+public class TheEmpressTarot extends TarotItem {
+
+	public static void handleOnPlayerTick(TickEvent.PlayerTickEvent event) {
+		Player player = event.player;
+		if (hasTarot(player, ItemRegistry.the_empress.get())) {
+			List<Animal> entities = player.level.getNearbyEntities(Animal.class, TargetingConditions.DEFAULT, player, player.getBoundingBox().inflate(Configuration.the_empress_range.get()));
+			for (Animal e : entities) {
+				if(e.canFallInLove() && e.getAge() == 0) {
+					e.setInLove(player);
+				}
+
+				if (e.isBaby()) {
+					e.ageUp(AgeableMob.getSpeedUpSecondsWhenFeeding(-e.getAge()), true);
+				}
+			}
+		}
+	}
+
+}
