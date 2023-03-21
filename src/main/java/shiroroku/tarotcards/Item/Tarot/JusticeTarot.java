0946556f09c3f85a7ef9c49283ed320b800d2fed
Shiroroku(Elise) <elseytd@gmail.com>
2023-03-21 10:32:45 -0700

    justice tarot crash fix

--- src/main/java/shiroroku/tarotcards/Item/Tarot/JusticeTarot.java
+++ src/main/java/shiroroku/tarotcards/Item/Tarot/JusticeTarot.java
@@ -5,21 +5,23 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import shiroroku.tarotcards.Item.TarotItem;
-import shiroroku.tarotcards.TarotCards;
 import shiroroku.tarotcards.Registry.ItemRegistry;
+import shiroroku.tarotcards.TarotCards;
 
 public class JusticeTarot extends TarotItem {
 
-	public static void handleOnHurt(LivingHurtEvent event) {
-		if (event.getSource().getEntity() != null && event.getSource().getEntity() instanceof LivingEntity attacker && event.getEntityLiving() instanceof Player player) {
-			if (hasTarot(player, ItemRegistry.justice.get())) {
-				attacker.hurt(DamageSource.playerAttack(player), event.getAmount());
-				TarotCards.LOGGER.debug("TAROT PASSIVE: {} - Returning damage", ItemRegistry.justice.get());
-				TarotCards.LOGGER.debug("From : {} [{}]", attacker, attacker.getHealth());
-				TarotCards.LOGGER.debug("To : {}", player);
-				TarotCards.LOGGER.debug("Amount : {}", event.getAmount());
-			}
-		}
-	}
+    private static final DamageSource justice = new DamageSource("justice").bypassArmor();
+
+    public static void handleOnHurt(LivingHurtEvent event) {
+        if (event.getSource().getEntity() != null && event.getSource().getEntity() instanceof LivingEntity attacker && event.getEntityLiving() instanceof Player player) {
+            if (!event.getSource().getMsgId().equals(justice.getMsgId()) && hasTarot(player, ItemRegistry.justice.get())) {
+                attacker.hurt(justice, event.getAmount());
+                TarotCards.LOGGER.debug("TAROT PASSIVE: {} - Returning damage", ItemRegistry.justice.get());
+                TarotCards.LOGGER.debug("From : {} [{}]", attacker, attacker.getHealth());
+                TarotCards.LOGGER.debug("To : {}", player);
+                TarotCards.LOGGER.debug("Amount : {}", event.getAmount());
+            }
+        }
+    }
 
 }
