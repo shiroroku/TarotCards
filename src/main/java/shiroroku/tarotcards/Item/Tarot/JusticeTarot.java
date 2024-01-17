90ed1a5aa56eb1f5d81aef1704b79d47d2b9bb20
shiroroku(Elise) <empresselise@proton.me>
2024-01-16 20:38:06 -0800

    fixed justice card

--- src/main/java/shiroroku/tarotcards/Item/Tarot/JusticeTarot.java
+++ src/main/java/shiroroku/tarotcards/Item/Tarot/JusticeTarot.java
@@ -5,7 +5,9 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -22,32 +24,37 @@ import java.util.List;
 
 public class JusticeTarot extends TarotItem {
 
-	public static ResourceKey<DamageType> JUSTICE = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(TarotCards.MODID, "justice"));
+    private static ResourceKey<DamageType> JUSTICE = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(TarotCards.MODID, "justice"));
 
-	public static void handleOnHurt(LivingHurtEvent event) {
-		//Make sure it is a living entity hurting a player
-		if (event.getSource().getEntity() instanceof LivingEntity attacker && event.getEntity() instanceof Player player) {
+    public static void handleOnHurt(LivingHurtEvent event) {
+        //Make sure it is a living entity hurting a player
+        if (event.getSource().getEntity() instanceof LivingEntity attacker && event.getEntity() instanceof Player player) {
 
-			//Damage taken from justice shouldnt be returned
-			if (event.getSource().is(JUSTICE)) {
-				return;
-			}
+            //Damage taken from justice shouldnt be returned
+            if (event.getSource().is(JUSTICE)) {
+                return;
+            }
 
-			if (hasTarot(player, ItemRegistry.justice.get())) {
-				float amount = (float) (event.getAmount() * Configuration.justice_damagemultiplier.get());
-				//todo justice damage source
-				//attacker.hurt(event.getEntity().damageSources().source(JUSTICE), amount);
+            if (hasTarot(player, ItemRegistry.justice.get())) {
+                float amount = (float) (event.getAmount() * Configuration.justice_damagemultiplier.get());
 
-				TarotCards.LOGGER.debug("TAROT PASSIVE: {} - Returning damage", ItemRegistry.justice.get());
-				TarotCards.LOGGER.debug("From : {}", player);
-				TarotCards.LOGGER.debug("To : {} [{}]", attacker, attacker.getHealth());
-				TarotCards.LOGGER.debug("Amount : {}", amount);
-			}
-		}
-	}
+                attacker.hurt(justice_damage(player), amount);
 
-	@Override
-	public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
-		tooltip.add(Component.translatable(this.getDescriptionId() + ".desc", String.valueOf(Configuration.justice_damagemultiplier.get() * 100)).withStyle(ChatFormatting.BLUE));
-	}
+                TarotCards.LOGGER.debug("TAROT PASSIVE: {} - Returning damage", ItemRegistry.justice.get());
+                TarotCards.LOGGER.debug("From : {}", player);
+                TarotCards.LOGGER.debug("To : {} [{}]", attacker, attacker.getHealth());
+                TarotCards.LOGGER.debug("Amount : {}", amount);
+            }
+        }
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.translatable(this.getDescriptionId() + ".desc", String.valueOf(Configuration.justice_damagemultiplier.get() * 100)).withStyle(ChatFormatting.BLUE));
+    }
+
+    private static DamageSource justice_damage(Entity entity) {
+        return new DamageSource(entity.level().registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(JUSTICE));
+    }
+
 }
