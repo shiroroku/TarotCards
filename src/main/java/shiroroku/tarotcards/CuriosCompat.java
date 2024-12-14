de33d0ff2ba2b7772be51c0bdb77121ec95d56a8
shiroroku(elise) <empresselise@proton.me>
2024-12-13 17:27:01 -0800

    tarot cards can now be put in the curio slot, closes #22

--- src/main/java/shiroroku/tarotcards/CuriosCompat.java
+++ src/main/java/shiroroku/tarotcards/CuriosCompat.java
@@ -1,6 +1,7 @@
 package shiroroku.tarotcards;
 
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -14,15 +15,22 @@ import top.theillusivec4.curios.api.SlotTypePreset;
 @Mod.EventBusSubscriber(modid = TarotCards.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CuriosCompat {
 
-	@SubscribeEvent
-	public static void enqueueIMC(InterModEnqueueEvent evt) {
-		InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CURIO.getMessageBuilder().size(1).build());
-	}
+    @SubscribeEvent
+    public static void enqueueIMC(InterModEnqueueEvent evt) {
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CURIO.getMessageBuilder().size(1).build());
+    }
 
-	public static ItemStack getTarotDeckCurio(Player player) {
-		if (CuriosApi.getCuriosHelper().findFirstCurio(player, ItemRegistry.tarot_deck.get()).isPresent()) {
-			return CuriosApi.getCuriosHelper().findFirstCurio(player, ItemRegistry.tarot_deck.get()).get().stack();
-		}
-		return null;
-	}
+    public static ItemStack getTarotDeckCurio(Player player) {
+        if (CuriosApi.getCuriosHelper().findFirstCurio(player, ItemRegistry.tarot_deck.get()).isPresent()) {
+            return CuriosApi.getCuriosHelper().findFirstCurio(player, ItemRegistry.tarot_deck.get()).get().stack();
+        }
+        return null;
+    }
+
+    public static ItemStack getTarotCardCurio(Player player, Item tarotItem) {
+        if (CuriosApi.getCuriosHelper().findFirstCurio(player, tarotItem).isPresent()) {
+            return CuriosApi.getCuriosHelper().findFirstCurio(player, tarotItem).get().stack();
+        }
+        return null;
+    }
 }
